7b1b786187d4137ad1b9a111d55a7c368c243766
wada <wada@slis.tsukuba.ac.jp>
2013-04-03 20:24:57 +0900

    update

--- si.r
+++ si.r
@@ -1,1 +1,38 @@
 #SI index in R
+
+#> class(k$cluster)
+#[1] "integer"
+#> class(k$centers)
+#[1] "matrix"
+#> class(k$totss)
+#[1] "numeric"
+#> class(k$withinss)
+#[1] "numeric"
+#> class(k$tot.withinss)
+#[1] "numeric"
+#> class(k$betweenss)
+#[1] "numeric"
+#> class(k$size)
+#[1] "integer"
+#> class(k)
+#[1] "kmeans"
+
+############WCD############
+#引数：
+#返り値：
+wcd <- function(){
+
+
+}
+
+
+
+############BCD############
+#引数：
+#返り値：
+bcd <- function(){
+
+
+}
+
+
