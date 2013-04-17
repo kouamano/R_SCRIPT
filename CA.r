ed7d179b2d4de8f81e2906011f67ff0398df8271
wada <wada@slis.tsukuba.ac.jp>
2013-04-17 23:12:38 +0900

    correct all

--- CA.r
+++ CA.r
@@ -1,5 +1,3 @@
-source("sf.r")
-
 CA1 <- function(N,R,Cn,rn){
 	rn = rn/R;
 	list(Cn,
---
+++ CA_loop.r
@@ -0,0 +1,33 @@
+source("sf.r")
+source("CA.r")
+
+CA_loop <- function(inputdata, loop){
+        ca_looped <- list()
+        datanum <- nrow(inputdata) -1
+
+        for (m in 1:loop){
+                ca_list <- list()
+
+                for (n in 1:datanum){
+						km <- kmeans(inputdata, n)
+						ca <- CA1(n, center_dis(inputdata), km$size, ca_wcd(n, addresult(inputdata,km,n),km))
+                        ca_list[length(ca_list)+1] <- ca[[5]]
+                }
+
+                for(y in 1:length(ca_list)){
+                        if (m ==1){
+                                ca_looped <- ca_list
+                        } else if (ca_looped[[y]] > ca_list[[y]]){
+                                ca_looped[[y]] <- ca_list[[y]]
+                        }
+                }
+
+        }
+        ca_looped[length(ca_looped)+1] <- ca_looped[[1]]
+        ca_looped
+}
+
+
+
+
+
