19e729af770f25578d3403a74e69749a606bfe1e
wada <wada@slis.tsukuba.ac.jp>
2013-05-15 21:07:55 +0900

    sc

--- sc.r
+++ sc.r
@@ -6,6 +6,12 @@ addresult <- function(data, km, k){
         listed
 }
 
+km1 <- function(data){
+	result <- kmenas(data,1)
+	result
+}
+
+
 all_R <- function(data){
 	km1 <- kmeans(data, 1)
 	sample_num <- nrow(data)
@@ -79,6 +85,65 @@ psc <- function(data,k){
 		pss <- pss * psing
 	}
 	pss
+}
+
+dkR <- function(dk, r){
+	result <- dk /r
+	result
+}
+
+Sk <- function(sk, rk, r, dim){
+	ni <- rk/r
+	iti <-  -(ni^dim)
+	result <- sk^iti
+
+#	kakko <- rk / r
+#	bunbo <- kakko^dim
+#	result <- sk / bunbo
+	result
+}
+
+dk <- function(km,km1,n){
+	kk <- km$centers[n,]
+	k1 <- km1$centers[1,]
+	re <- rbind(kk,k1)
+	d <- dist(re)
+	result <- d[1]
+	result
+}
+
+
+siguma <- function(dk, r, sk, rk, dim){
+	mae <- dkR(dk, r)
+	ato <- Sk(sk, rk, r, dim)
+	result <- mae * ato
+	result
+}
+
+a_index <- function(data,k,km1){
+	km <- kmeans(data,k)
+	addr <- addresult(data, km, k)
+
+	dim <- ncol(data)
+	r <- all_R(data)
+
+	result <- 0
+	for(i in 1:k){
+		sk <- nrow(addr[[2]][[i]])
+		dk <- dk(km,km1,i)
+		rk <- cluster_r(km$centers[i,], addr ,i)
+
+		sig <- siguma(dk, r, sk, rk, dim)
+		result <- sig + result
+	}
+	kbun <- result / k
+	kbun
+}
+
+amano_index <- function(data,k){
+	k1 <- kmeans(data,1)
+	result <- a_index(data, k, k1)
+	result
 }
 
 
