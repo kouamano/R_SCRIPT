81eee3132d521794208b5c6388627ea215aacf1f
wada <wada@slis.tsukuba.ac.jp>
2013-06-05 22:21:50 +0900

    add

--- index.r
+++ index.r
@@ -1,190 +1,98 @@
+﻿#正解データがついたデータを受けとって，正解ついたまま各クラスタごとに分割
+#[[1]]で各クラスタのデータフレームにアクセス
 split_by_result <- function(data){
 		n <- ncol(data)
         each_cluster <- split(data, data[n])
         each_cluster
 }
-#[[1]]�Ńf�[�^�t���[���ɃA�N�Z�X
 
+#正解つきデータを受け取って，全データの中心をもとめる
+#numericを返す
 Ztot <- function(data){
 	n <- ncol(data)
-	cuted <- data[-d]
+	cuted <- data[-n]
 	me <- colMeans(cuted)
 	me
 }
 
-
-
-
-
-
-Zi <- function(listx){
-	X <- list()
-	for(i in listx) X <- append(X, Ztot(i))
+#split_by_resultの結果を受け取って，各クラスタごとの中心を返す
+#data.frameを返す，[n,]でクラスタを指定
+Zk <- function(listdata){
+	X <- data.frame()
+	for(i in listdata){
+		X <- rbind(X, Ztot(i))
+	}
 	X
 }
 
-
-all_R <- function(data){
-	km1 <- kmeans(data, 1)
-	sample_num <- nrow(data)
-	co <- 0
-	
-	for (i in 1:sample_num){
-		bind_center <- rbind(data[i,], km1$centers)
-        d_center <- dist(bind_center)
-        co <- co + d_center[1]
-    }
-	aR <- co / sample_num
-	aR
-}
-
-cluster_r <- function(kcenter,listed,k_number){
-	count <- 0
-	num1 <- nrow(listed[[2]][[k_number]])
-
-	for(i in 1:num1){
-		row_1 <- listed[[2]][[k_number]][i,]
-        len <- length(row_1)
-        cutlast <- row_1[, -len[1]]
-        cluc <- rbind(kcenter, cutlast)
-        dis <- dist(cluc)
-		count <- count + dis[1]
+#split_by_resultの結果を受け取って，各クラスタごとの要素数を返す
+#data.frameを返す，[n,]でクラスタを指定
+nk <- function(listdata){
+	X <- data.frame()
+	for(i in listdata){
+		X <- rbind(X, nrow(i))
 	}
-	clr <- count / num1
-	clr
+	X
 }
 
-single_cl <- function(data,kcenter,num,addlist,allr,allsamplenum,s){
-	clr <- cluster_r(kcenter, addlist, num)
-	single <- (clr / allr +1) / (s / allsamplenum)
-	single
+#Zk,Ztotの結果を受け取って，各クラスタ重心と全体重心の距離を返す
+#matrixを返すk[n]で指定
+dk <- function(zk_result, ztot_result){
+	X <- rbind(zk_result, ztot_result)
+	Y <- as.matrix(dist(X))
+	n<- nrow(Y)
+	Y <- Y[n,-n]
+	Y
 }
 
+#Zk,nk,splitの結果を受け取って，各rkの結果を返す
+#matrixを返す，k[n]で指定
+rk <-function(zk_result, nk_result, listdata){
+	x <- 1
+	zk_result <- as.matrix(zk_result)
+	all <- matrix()
 
-Psingle_cl <- function(data,kcenter,num,addlist,allr,allsamplenum,s){
-	clr <- cluster_r(kcenter, addlist, num)
-	psin <- s^(-(clr/allr))
-	psin
-}
-
-
-sc <- function(data,k){
-	km <- kmeans(data,k)
-	addr <- addresult(data, km, k)
-	allr <- all_R(data)
-	allsam <- nrow(data)
-
-
-	ss <- 0
-	for(i in 1:k){
-		sk <- nrow(addr[[2]][[i]])
-		sing <- single_cl(data,km$centers[i,],i,addr,allr,allsam,sk)
-		ss <- ss + sing
+	for(n in listdata){
+		n <- as.matrix(n)
+		nc <- ncol(n)
+		rc <- n[,-nc]
+		bn <- rbind(rc, zk_result[x,])
+		Y <- as.matrix(dist(bn))
+		m<- nrow(Y)
+		Y <- Y[m,-m]
+		Y <- sum(Y)
+		Z <- Y / nk_result[x,]
+		all <- rbind(all, Z)
+		x <- x+1
 	}
-	ss
+	all[-1]
 }
 
-psc <- function(data,k){
-	km <- kmeans(data,k)
-	addr <- addresult(data, km, k)
-	allr <- all_R(data)
-	allsam <- nrow(data)
-
-	pss <- 1
-	for(i in 1:k){
-		sk <- nrow(addr[[2]][[i]])
-		psing <- Psingle_cl(data,km$centers[i,],i,addr,allr,allsam,sk)
-		pss <- pss * psing
-	}
-	pss
-}
-
-dkR <- function(dk, r){
-	result <- dk /r
-	result
-}
-
-Sk <- function(sk, rk, r, dim){
-	ni <- rk/r
-	iti <-  -(ni^dim)
-	result <- sk^iti
-
-#	kakko <- rk / r
-#	bunbo <- kakko^dim
-#	result <- sk / bunbo
-	result
-}
-
-dk <- function(km,km1,n){
-	kk <- km$centers[n,]
-	k1 <- km1$centers[1,]
-	re <- rbind(kk,k1)
-	d <- dist(re)
-	result <- d[1]
-	result
+#Ztotと，データを入れるとサンプルの平均半径
+#数値が帰ってくる
+Ra <- function(ztot_result, data){
+	n <- nrow(data)
+	i <- as.matrix(data)
+	ii <- ncol(i)
+	ri <- i[,-ii]
+	bn <- rbind(ri, ztot_result)
+	Y <- as.matrix(dist(bn))
+	m<- nrow(Y)
+	Y <- Y[m,-m]
+	Y <- sum(Y)
+	Y <- Y / n
+	Y
 }
 
 
-siguma <- function(dk, r, sk, rk, dim){
-	mae <- dkR(dk, r)
-	ato <- Sk(sk, rk, r, dim)
-	result <- mae * ato
-	result
-}
 
-a_index <- function(data,k,km1){
-	km <- kmeans(data,k)
-	addr <- addresult(data, km, k)
 
-	dim <- ncol(data)
-	r <- all_R(data)
 
-	result <- 0
-	for(i in 1:k){
-		sk <- nrow(addr[[2]][[i]])
-		dk <- dk(km,km1,i)
-		rk <- cluster_r(km$centers[i,], addr ,i)
 
-		sig <- siguma(dk, r, sk, rk, dim)
-		result <- sig + result
-	}
-	kbun <- result / k
-	kbun
-}
 
-amano_index <- function(data,k){
-	k1 <- kmeans(data,1)
-	result <- a_index(data, k, k1)
-	result
-}
 
-amano_index_loop <- function(inputdata,loop){
-	a_loop <- list()
-#	datanum <- nrow(inputdata) -1
-	datanum <- 10
 
-	k1 <- kmeans(inputdata,1)
 
-	for(m in 1:loop){
-		a_list <- list()
-
-		for(n in 1:datanum){
-			a_result <- a_index(inputdata,n,k1)
-			a_list[length(a_list)+1] <- a_result
-		}
-		
-		if(m == 1){
-			a_loop <- a_list
-		}else{
-			for (y in 1:length(a_list)){
-				if(a_loop[[y]] < a_list[[y]]){
-					a_loop[[y]] <- a_list[[y]]
-				}
-			}
-		}
-	}
-	a_loop
-}
 
 
 
