83957765323a5e93b16c8ce65c1af2c3e3368fb2
Kou AMANO <amano.au1@gmail.com>
2014-01-22 20:17:34 +0900

    rev

--- medoid.r
+++ medoid.r
@@ -1,4 +1,4 @@
-medoid <- function(mat){
+medoidc <- function(mat){
 	pos = 1;
 	minv = 0;
 	#print(nrow(mat));
@@ -16,4 +16,26 @@ medoid <- function(mat){
 	pos=which(targetList == min(targetList));
 	#print(pos);
 	return(list(pos,mat[pos,]));
-}
+};
+
+medoidd <- function(mat){
+	pos = 0;
+	#minv = 0;
+	#print(nrow(mat));
+	#v = c();
+	tmat = t(mat);
+	#for( i in 1:nrow(tmat) ){
+		#print(i);
+		#print(mean(tmat[i,]));
+		#v=cbind(v,mean(tmat[i,]));
+	#}
+	#print(v);
+	#rmat=rbind(mat,v);
+	#print(dist(rmat));
+	#targetList=as.matrix(dist(rmat))[nrow(mat)+1,-nrow(mat)-1];
+	targetList=as.matrix(dist(tmat));
+	print(targetList);
+	pos=which(targetList == min(targetList));
+	#print(pos);
+	#return(list(pos,mat[pos,]));
+};
